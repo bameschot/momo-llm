1a8d0b734f9e6fd925319fd8ecabde7bfbda5500
bameschot <53991953+bameschot@users.noreply.github.com>
2025-03-18 20:11:26 +0100

    disable compile autotune

--- TrainModel.py
+++ TrainModel.py
@@ -169,7 +169,7 @@ def trainModel(
         if torch.mps.is_available():
             torch._dynamo.config.suppress_errors = True
         torch.set_float32_matmul_precision('high')
-        model = torch.compile(model,mode="max-autotune")
+        model = torch.compile(model)#,mode="max-autotune")
     
     print(f"Training model on {device}")
 
