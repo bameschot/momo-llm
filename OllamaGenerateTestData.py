9507fe012a3dd8a5fed5ff13fc68ed93de48c059
bameschot <ameschot.b@gmail.com>
2025-12-06 10:37:18 +0100

    added conversation seperator

--- OllamaGenerateTestData.py
+++ OllamaGenerateTestData.py
@@ -90,7 +90,7 @@ for genIdx in range(p_numberOfGenerations):
     messageContent = response.message.content+'\n'
     processedContent = processRawTextLineWithReplacements(messageContent,False,replacementDefinition)
     charactersWritten += len(processedContent)
-    text += processedContent + "\n=====================\n"
+    text += processedContent + "=====================\n"
 
     if genIdx > 0 and genIdx % p_printInterval == 0:
         print(processedContent)
